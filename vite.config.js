8f3a154bdb66bedad9bfed2b60190a3b344b4a72
Shirleen ♡ <shirleenkosgei306@gmail.com>
2025-11-25 14:57:00 +0300

    Add files via upload
    new working main

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite';
+=======
+>>>>>>> 6ff1f53 (Initial commit - my version)
 
 export default defineConfig({
     plugins: [
@@ -8,6 +11,9 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+<<<<<<< HEAD
         tailwindcss(),
+=======
+>>>>>>> 6ff1f53 (Initial commit - my version)
     ],
 });
